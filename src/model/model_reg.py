009494015aa9999453093f077ccefe58e269803e
sfgrahman <sfgrahman35@gmail.com>
2025-02-18 10:15:17 +0600

    model staging

--- src/model/model_reg.py
+++ src/model/model_reg.py
@@ -46,7 +46,7 @@ reg = mlflow.register_model(model_uri, model_name)
 model_version = reg.version  # Get the registered model version
 
 # Transition the model version to Staging
-new_stage = "Production"
+new_stage = "Staging"
 
 client.transition_model_version_stage(
     name=model_name,
